a8c001d79364d0f7a1a6cbe31702124141eeb965
sjorstweet <sjorshekerman@gmail.com>
2026-09-23 15:24:08 +0200

    initialize project

---
+++ src/test/java/com/sjors37/spring_ai_mcp_git_analyzer/SpringAiMcpGitAnalyzerApplicationTests.java
@@ -0,0 +1,13 @@
+package com.sjors37.spring_ai_mcp_git_analyzer;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class SpringAiMcpGitAnalyzerApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
